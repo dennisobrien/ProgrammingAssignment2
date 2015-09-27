145b44f459bece9e36a1010bbf67c4a2f33827bb
Dennis O'Brien <dennis@dennisobrien.net>
2015-09-26 19:19:28 -0700

    Coursera R Programming - assignment 2 - functionally complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache the results of a matrix inversion.
+## This wraps a matrix in a list with a few getter/setter methods.
 
-## Write a short comment describing this function
+## Wrap the matrix in a list.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    set_inverse <- function(inverse) m <<- inverse
+    get_inverse <- function() m
+    list(set = set, get = get,
+         set_inverse = set_inverse,
+         get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## Solve the matrix inversion and cache the result in the data structure.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$get_inverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$set_inverse(m)
+    m
 }
