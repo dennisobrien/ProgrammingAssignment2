09867dbf39554d0a863d7e8a2cbf1afdcd6babbd
Dennis O'Brien <dennis@dennisobrien.net>
2015-09-26 19:33:49 -0700

    be more explicit with the overall documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,21 @@
 ## Cache the results of a matrix inversion.
 ## This wraps a matrix in a list with a few getter/setter methods.
+## Example:
+##      Object construction:
+## > x <- makeCacheMatrix(matrix(rnorm(5*5), 5, 5))
+##      Get the wrapped matrix:
+## > x$get()
+##      Get the inverse matrix:
+## > y <- cacheSolve(x)
+##      Reset the matrix and invalidate the cache.  This should be the only way to modify the contents of the matrix.
+## > x$set(matrix(rnorm(5*5), 5, 5))
 
-## Wrap the matrix in a list.
 
+## Wrap the matrix in a list data structure with the following methods.
+##     `get` - Get the matrix data.
+##     `set` - Set the matrix data and invalidate the cache.
+##     `set_inverse` - set the inverse matrix. To be called by `cacheSolve`
+##     `get_inverse` - retrieve the cached inverse matrix or NULL if it has not been computed.  To be called by `cacheSolve`.
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
@@ -19,7 +32,6 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Solve the matrix inversion and cache the result in the data structure.
-
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     m <- x$get_inverse()
